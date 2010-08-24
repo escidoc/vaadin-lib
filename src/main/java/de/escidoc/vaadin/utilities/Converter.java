328795e7c8aa343b9d7750e1ee05eb3f584daabf
ASP <ASP>
2010-08-24 06:30:04 +0000

    Added a method for different types of results. Not testet yet!

--- src/main/java/de/escidoc/vaadin/utilities/Converter.java
+++ src/main/java/de/escidoc/vaadin/utilities/Converter.java
@@ -4,7 +4,10 @@
 package de.escidoc.vaadin.utilities;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 import org.joda.time.DateTime;
 
@@ -24,6 +27,25 @@ public class Converter {
         Date date = dateTime.toDate();
         SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
         return sdf.format(date);
+    }
+
+    /**
+     * Vaadin uses an Object to return either a single value
+     * 
+     * @param o
+     *            the return object from a tree, combobox, list, ....
+     * @return a list of selected objects.
+     */
+    @SuppressWarnings("unchecked")
+    public static synchronized List<Object> getSelected(Object o) {
+        final List<Object> selected = new ArrayList<Object>();
+        if (o instanceof Set) {
+            selected.addAll((Set) o); // More than one result...
+        }
+        else if (o instanceof Object) { // Just one selected.
+            selected.add(o);
+        }
+        return selected;
     }
 
 }
