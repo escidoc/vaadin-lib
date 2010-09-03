23c8389446a85450f2b508c896ce7c7d37d1c174
ASP <ASP>
2010-09-03 17:03:47 +0000

    Added constants for IMenuItem.

---
+++ src/main/java/de/escidoc/vaadin/Constants.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.escidoc.vaadin;
+
+
+/**
+ * @author ASP
+ * 
+ */
+public class Constants {
+    public static final String CREATED_BY = "createdBy";
+
+    public static final String CREATED_ON = "createdOn";
+
+    public static final String MODIFIED_BY = "modifiedBy";
+
+    public static final String MODIFIED_ON = "modifiedOn";
+
+}
