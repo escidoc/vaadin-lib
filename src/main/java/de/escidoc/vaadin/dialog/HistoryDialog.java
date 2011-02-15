f744b93f3ab791e5d23cf839a92ff6988c84c248
ASP <ASP>
2011-02-15 16:10:23 +0000

    Removed findbugs warnings.

--- src/main/java/de/escidoc/vaadin/dialog/HistoryDialog.java
+++ src/main/java/de/escidoc/vaadin/dialog/HistoryDialog.java
@@ -40,7 +40,8 @@ public class HistoryDialog extends Window implements Button.ClickListener {
      */
     public HistoryDialog(final Window mainWindow, final String caption,
         final POJOItem<?> pojo, final String className) {
-        this(mainWindow, caption, 600, 300, pojo, className);
+        this(mainWindow, caption, Constants.ERROR_DIALOG_WIDTH,
+            Constants.ERROR_DIALOG_HEIGTH, pojo, className);
     }
 
     /**
@@ -62,12 +63,13 @@ public class HistoryDialog extends Window implements Button.ClickListener {
     public HistoryDialog(final Window mainWindow, final String caption,
         int width, int height, final POJOItem<?> pojo, final String className) {
         this.mainWindow = mainWindow;
-        super.setWidth(width + "px");
-        super.setHeight(height + "px");
+        super.setWidth(width + Constants.PX);
+        super.setHeight(height + Constants.PX);
         super.setCaption(caption);
         super.setModal(true);
         // created
-        final SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        final SimpleDateFormat sdf =
+            new SimpleDateFormat(Constants.DATE_FORMAT);
         final AbstractComponent createdByField =
             LayoutHelper.createElement(className, pojo, true,
                 Constants.CREATED_BY);
@@ -80,7 +82,8 @@ public class HistoryDialog extends Window implements Button.ClickListener {
         }
 
         layout.addComponent(LayoutHelper.create("Created:", " by ", new Label(
-            sdf.format(creationDate)), createdByField, 100, 80, false));
+            sdf.format(creationDate)), createdByField, 100,
+            Constants.TEXT_WIDTH, false));
 
         // modified
         final AbstractComponent modifiedByField =
@@ -92,14 +95,16 @@ public class HistoryDialog extends Window implements Button.ClickListener {
             modificationDate = new Date();
         }
         layout.addComponent(LayoutHelper.create("Modified:", " by ", new Label(
-            sdf.format(modificationDate)), modifiedByField, 100, 80, false));
-        Button button = new Button("OK", this);
+            sdf.format(modificationDate)), modifiedByField, 100,
+            Constants.TEXT_WIDTH, false));
+        Button button = new Button(Constants.OK, this);
         layout.addComponent(LayoutHelper.create("", button, 10, false));
         super.addComponent(layout);
     }
 
     @Override
-    public void buttonClick(ClickEvent event) {
+    public final void buttonClick(ClickEvent event) {
+        
         mainWindow.removeWindow(HistoryDialog.this);
     }
 
