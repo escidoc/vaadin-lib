8a9ab48eb2c213257d96866fa101b879b85e374f
ASP <ASP>
2011-03-22 08:37:48 +0000

    Added a validation for combo box.

--- src/main/java/de/escidoc/vaadin/validator/EmptyFieldValidator.java
+++ src/main/java/de/escidoc/vaadin/validator/EmptyFieldValidator.java
@@ -4,6 +4,7 @@
 package de.escidoc.vaadin.validator;
 
 import com.vaadin.terminal.UserError;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.NativeSelect;
 import com.vaadin.ui.TextField;
@@ -54,6 +55,28 @@ public final class EmptyFieldValidator {
      */
     public static synchronized boolean isValid(
         final ListSelect list, final String message) {
+
+        if (list.getItemIds() != null && list.getItemIds().size() > 0) {
+            list.setComponentError(null);
+            return true;
+        }
+
+        list.setComponentError(null);
+        list.setComponentError(new UserError(message));
+        return false;
+    }
+    /**
+     * A simple validator to test, if the field is filled.
+     * 
+     * @param list
+     *            The list to test.
+     * @param message
+     *            The message that should be shown (as a tooltip) if the result
+     *            is bad.
+     * @return true if the field is filled, otherwise false.
+     */
+    public static synchronized boolean isValid(
+        final ComboBox list, final String message) {
 
         if (list.getItemIds() != null && list.getItemIds().size() > 0) {
             list.setComponentError(null);
