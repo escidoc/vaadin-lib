dee850cd64a66815b54cf015773389686161f766
BSZ <BSZ>
2011-07-25 11:11:24 +0000

    BWELABS-54 Empty Combobox validation method is fixed.

--- src/main/java/de/escidoc/vaadin/validator/EmptyFieldValidator.java
+++ src/main/java/de/escidoc/vaadin/validator/EmptyFieldValidator.java
@@ -26,15 +26,12 @@ public final class EmptyFieldValidator {
      * @param textField
      *            The textfield to test.
      * @param message
-     *            The message that should be shown (as a tooltip) if the result
-     *            is bad.
+     *            The message that should be shown (as a tooltip) if the result is bad.
      * @return true if the field is filled, otherwise false.
      */
-    public static synchronized boolean isValid(
-        final TextField textField, final String message) {
+    public static synchronized boolean isValid(final TextField textField, final String message) {
 
-        if (!(textField.getValue() != null && ((String) textField.getValue())
-            .trim().length() > 0)) {
+        if (!(textField.getValue() != null && ((String) textField.getValue()).trim().length() > 0)) {
             textField.setComponentError(null);
             textField.setComponentError(new UserError(message));
             return false;
@@ -49,34 +46,10 @@ public final class EmptyFieldValidator {
      * @param list
      *            The list to test.
      * @param message
-     *            The message that should be shown (as a tooltip) if the result
-     *            is bad.
+     *            The message that should be shown (as a tooltip) if the result is bad.
      * @return true if the field is filled, otherwise false.
      */
-    public static synchronized boolean isValid(
-        final ListSelect list, final String message) {
-
-        if (list.getItemIds() != null && list.getItemIds().size() > 0) {
-            list.setComponentError(null);
-            return true;
-        }
-
-        list.setComponentError(null);
-        list.setComponentError(new UserError(message));
-        return false;
-    }
-    /**
-     * A simple validator to test, if the field is filled.
-     * 
-     * @param list
-     *            The list to test.
-     * @param message
-     *            The message that should be shown (as a tooltip) if the result
-     *            is bad.
-     * @return true if the field is filled, otherwise false.
-     */
-    public static synchronized boolean isValid(
-        final ComboBox list, final String message) {
+    public static synchronized boolean isValid(final ListSelect list, final String message) {
 
         if (list.getItemIds() != null && list.getItemIds().size() > 0) {
             list.setComponentError(null);
@@ -88,6 +61,27 @@ public final class EmptyFieldValidator {
         return false;
     }
 
+    /**
+     * A simple validator to test, if the field is filled.
+     * 
+     * @param list
+     *            The list to test.
+     * @param message
+     *            The message that should be shown (as a tooltip) if the result is bad.
+     * @return true if the field is filled, otherwise false.
+     */
+    public static synchronized boolean isValid(final ComboBox list, final String message) {
+
+        if (list.getItemIds() != null && list.getItemIds().size() > 0 && list.getValue() != null
+            && ((String) list.getValue()).trim().length() > 0) {
+            list.setComponentError(null);
+            return true;
+        }
+
+        list.setComponentError(null);
+        list.setComponentError(new UserError(message));
+        return false;
+    }
 
     /**
      * A simple validator to test, if the field is filled.
@@ -95,14 +89,12 @@ public final class EmptyFieldValidator {
      * @param list
      *            The list to test.
      * @param message
-     *            The message that should be shown (as a tooltip) if the result
-     *            is bad.
+     *            The message that should be shown (as a tooltip) if the result is bad.
      * @return true if the field is filled, otherwise false.
      */
-    public static synchronized boolean isValid(
-        final NativeSelect select, final String blank, final String message) {
+    public static synchronized boolean isValid(final NativeSelect select, final String blank, final String message) {
 
-        if (select != null && select.getValue()!= null && !((String)select.getValue()).equals(blank)) {
+        if (select != null && select.getValue() != null && !((String) select.getValue()).equals(blank)) {
             select.setComponentError(null);
             return true;
         }
